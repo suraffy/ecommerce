91a124541a4a34bce8a95be0459ec5f3878bf197
Surafel Araya <surafelay@gmail.com>
2023-05-18 07:16:34 +0300

    Reduce HTML Code

--- app.js
+++ app.js
@@ -7,45 +7,100 @@
 // Add products to cart, show number of products in cart
 // Display products in cart, and calculate the total
 
-const filterList = document.querySelector('.filter-list');
-const pageTitle = document.querySelector('.page-title');
+const filterList = document.querySelector(".filter-list");
+const pageTitle = document.querySelector(".page-title");
 
-const searchEl = document.getElementById('searchEl');
-const searchBtn = document.getElementById('searchBtn');
+const searchEl = document.getElementById("searchEl");
+const searchBtn = document.getElementById("searchBtn");
 
-const productsList = document.querySelector('.products-list');
-const productItems = document.querySelectorAll('.product-item');
+const productsList = document.querySelector(".products-list");
+const productItems = document.querySelectorAll(".product-item");
 
-const cart = document.querySelector('.cart');
-const cartNumber = document.querySelector('.cartNumber');
+const cart = document.querySelector(".cart");
+const cartNumber = document.querySelector(".cartNumber");
 let cartNumValue = 0;
 
-const cartDetails = document.querySelector('.cart-details');
-const productsInCart = document.querySelector('.products-in-cart');
-const cartEmpty = document.querySelector('.cart-empty');
-const calcTotalBtn = document.querySelector('#calculate-total');
-const totalPrice = document.querySelector('.total-price');
+const cartDetails = document.querySelector(".cart-details");
+const productsInCart = document.querySelector(".products-in-cart");
+const cartEmpty = document.querySelector(".cart-empty");
+const calcTotalBtn = document.querySelector("#calculate-total");
+const totalPrice = document.querySelector(".total-price");
 const cartForm = document.forms.cartDetailsForm;
 
 const filterValues = new Set();
 
-filterList.addEventListener('click', filterProducts);
-searchEl.addEventListener('input', searchProducts);
-searchBtn.addEventListener('click', searchProducts);
-productsList.addEventListener('click', addToCart);
-cartForm.addEventListener('submit', calcTotal);
+filterList.addEventListener("click", filterProducts);
+searchEl.addEventListener("input", searchProducts);
+searchBtn.addEventListener("click", searchProducts);
+productsList.addEventListener("click", addToCart);
+cartForm.addEventListener("submit", calcTotal);
 
-cart.addEventListener('click', () => {
-  cartDetails.classList.toggle('show-cart-details');
+const products = [
+  {
+    type: "fruit",
+    name: "Orange",
+    price: 2.6,
+    thumbnail: "orange vlc.png",
+  },
+  {
+    type: "fruit",
+    name: "Strawbery",
+    price: 2.99,
+    thumbnail: "strawbery vlc.png",
+  },
+  {
+    type: "fruit",
+    name: "Banana",
+    price: 1.99,
+    thumbnail: "banana vlc.png",
+  },
+  {
+    type: "fruit",
+    name: "Leaft",
+    price: 0.6,
+    thumbnail: "leaf.jpg",
+  },
+  {
+    type: "fruit",
+    name: "Leaft",
+    price: 0.6,
+    thumbnail: "leaf.jpg",
+  },
+];
+
+products.forEach((product) => {
+  addProducts(product);
+});
+
+function addProducts(product) {
+  productsList.insertAdjacentHTML(
+    "beforeend",
+    `
+  <div class="product-item" data-product-type="${product.type}" data-product-name="${product.name}" data-product-price=${product.price}>
+  <div class="thumbnail">
+    <img src="${product.thumbnail}" alt="${product.name} Image">
+  </div>
+  <div class="info">
+    <div class="product-name">${product.name}</div>
+    <div class="product-price">$${product.price}</div>
+    <button class="add-to-cart">Add to cart</button>
+  </div>
+  </div>
+  `
+  );
+}
+
+cart.addEventListener("click", () => {
+  cartDetails.classList.toggle("show-cart-details");
 });
 
 function filterProducts() {
   const filterEl = event.target;
-  if (filterEl.tagName !== 'LI') return;
+  if (filterEl.tagName !== "LI") return;
 
-  filterEl.classList.toggle('filterBgColor');
+  filterEl.classList.toggle("filterBgColor");
 
-  const filterType = filterEl.getAttribute('data-filter-type');
+  const filterType = filterEl.getAttribute("data-filter-type");
 
   if (filterValues.has(filterType)) {
     filterValues.delete(filterType);
@@ -53,75 +108,75 @@ function filterProducts() {
     filterValues.add(filterType);
   }
 
-  if (filterValues.has('all') || filterValues.size === 0) {
-    for (product of productItems) product.style.display = 'block';
-    pageTitle.textContent = 'All Products';
+  if (filterValues.has("all") || filterValues.size === 0) {
+    for (product of productItems) product.style.display = "block";
+    pageTitle.textContent = "All Products";
     return;
   }
 
   for (product of productItems) {
-    if (filterValues.has(product.getAttribute('data-product-type'))) {
-      product.style.display = 'block';
+    if (filterValues.has(product.getAttribute("data-product-type"))) {
+      product.style.display = "block";
     } else {
-      product.style.display = 'none';
+      product.style.display = "none";
     }
   }
 
-  pageTitle.textContent = '';
+  pageTitle.textContent = "";
   for (f of filterValues) {
-    if (f === 'all') continue;
+    if (f === "all") continue;
 
     f = f.replace(f[0], f[0].toUpperCase());
-    pageTitle.textContent += f + ', ';
+    pageTitle.textContent += f + ", ";
   }
 }
 
 function searchProducts() {
-  const searchPatt = new RegExp(searchEl.value, 'i');
+  const searchPatt = new RegExp(searchEl.value, "i");
 
   for (product of productItems) {
-    const productName = product.getAttribute('data-product-name');
+    const productName = product.getAttribute("data-product-name");
 
     if (
       searchPatt.test(productName) &&
       (!filterValues.size ||
-        filterValues.has(product.getAttribute('data-product-type')))
+        filterValues.has(product.getAttribute("data-product-type")))
     ) {
-      product.style.display = 'block';
-    } else product.style.display = 'none';
+      product.style.display = "block";
+    } else product.style.display = "none";
   }
 }
 
 function addToCart(event) {
-  if (event.target.classList.contains('add-to-cart')) {
+  if (event.target.classList.contains("add-to-cart")) {
     const btn = event.target;
     const product = btn.parentNode.parentNode;
-    const prdName = product.getAttribute('data-product-name');
-    const prdPrice = product.getAttribute('data-product-price');
+    const prdName = product.getAttribute("data-product-name");
+    const prdPrice = product.getAttribute("data-product-price");
 
-    if (!btn.classList.contains('added')) {
-      btn.textContent = 'Remove from cart';
-      btn.classList.add('added');
+    if (!btn.classList.contains("added")) {
+      btn.textContent = "Remove from cart";
+      btn.classList.add("added");
       cartNumValue++;
 
-      const li = document.createElement('li');
+      const li = document.createElement("li");
       li.textContent = `${prdName}, $${prdPrice}`;
-      li.setAttribute('data-product-name', prdName);
-      li.setAttribute('data-product-price', prdPrice);
+      li.setAttribute("data-product-name", prdName);
+      li.setAttribute("data-product-price", prdPrice);
       productsInCart.append(li);
 
       li.insertAdjacentHTML(
-        'beforeend',
+        "beforeend",
         `<input type="Number" class="product-quantity" min="1" max="999" step="1" required placeholder="Quantity">`
       );
     } else {
-      btn.textContent = 'Add to cart';
-      btn.classList.remove('added');
+      btn.textContent = "Add to cart";
+      btn.classList.remove("added");
       cartNumValue--;
 
       const lis = productsInCart.children;
       for (li of lis) {
-        if (li.getAttribute('data-product-name') === prdName) {
+        if (li.getAttribute("data-product-name") === prdName) {
           li.remove();
           break;
         }
@@ -131,14 +186,14 @@ function addToCart(event) {
     cartNumber.textContent = cartNumValue;
 
     if (productsInCart.childElementCount === 0) {
-      cartEmpty.style.display = 'block';
-      calcTotalBtn.style.display = 'none';
-      totalPrice.style.display = 'none';
-      totalPrice.textContent = '';
+      cartEmpty.style.display = "block";
+      calcTotalBtn.style.display = "none";
+      totalPrice.style.display = "none";
+      totalPrice.textContent = "";
     } else {
-      cartEmpty.style.display = 'none';
-      calcTotalBtn.style.display = 'block';
-      totalPrice.style.display = 'block';
+      cartEmpty.style.display = "none";
+      calcTotalBtn.style.display = "block";
+      totalPrice.style.display = "block";
     }
   }
 }
@@ -149,14 +204,14 @@ function calcTotal(event) {
   let total = 0;
   for (product of productsInCart.children) {
     total +=
-      product.getAttribute('data-product-price') * product.lastChild.value;
+      product.getAttribute("data-product-price") * product.lastChild.value;
   }
 
   total = Math.round(total * 100) / 100;
   totalPrice.textContent = `Total = $${total}`;
-  totalPrice.style.animation = 'animateBg 1s';
+  totalPrice.style.animation = "animateBg 1s";
 
   setTimeout(() => {
-    totalPrice.style.animation = '';
+    totalPrice.style.animation = "";
   }, 1000);
 }
